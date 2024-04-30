4e0a1f7228e6cf886a9e4b3739d079ed3320931c
tin900 <t.iwan@vu.nl>
2024-04-30 11:23:33 +0200

    add: script to fetch data with vvcanvas

---
+++ 00_download/download_canvas.R
@@ -0,0 +1,26 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## R code for Education Analytics Vrije Universiteit Amsterdam
+## Copyright 2024 VU
+## Web Page: http://www.vu.nl
+## Contact:
+##
+##' *INFO*:
+## 1) ___
+##
+## ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+dfCourses <- vvcanvas::get_all_courses(canvas)
+
+dfStudents <- dfCourses %>%
+  pull(course.id) %>%
+  map_dfr(~ vvcanvas::get_course_students(canvas, .x))
+
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## WRITE & CLEAR ####
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+##' *TODO*
+##' Write to file
+
+vusa::clear_script_objects()
