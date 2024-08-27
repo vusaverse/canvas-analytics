745e23626d47c30ecda755193cd57e38888ebccd
Tomer <t.iwan@vu.nl>
2024-08-27 12:01:50 +0200

    add: script to fetch all courses in index

---
+++ 00_download/download_all_courses.R
@@ -0,0 +1,22 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## R code for Education Analytics Vrije Universiteit Amsterdam
+## Copyright 2024 VU
+## Web Page: http://www.vu.nl
+## Contact: vu-analytics@vu.nl
+## Distribution outside of the VU: yes.
+##
+##' *INFO*:
+## 1) ___
+##
+## ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+dfCourses <- vvcanvas::get_all_courses(canvas)
+
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## WRITE & CLEAR ####
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+write_file_proj(dfCourses, "CAN_Index")
+
+clear_script_objects()
