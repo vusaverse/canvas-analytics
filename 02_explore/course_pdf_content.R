f84b99fa3fa3e0691a248cbbae3d671c75b8059b
Tomer <t.iwan@vu.nl>
2024-10-24 15:46:44 +0200

    feat: explore contents of pdf files

---
+++ 02_explore/course_pdf_content.R
@@ -0,0 +1,32 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## R code for Education Analytics Vrije Universiteit Amsterdam
+## Copyright 2024 VU
+## Web Page: http://www.vu.nl
+## Contact: vu-analytics@vu.nl
+##
+##' *INFO*:
+## 1) Get aggregated student counts in course
+##
+## ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+dfPDF <- read_file_proj("CAN_PDF_text")
+dfCourse_details <- read_file_proj("CAN_Course_details")
+
+
+dfCourse_to_join <- dfCourse_details %>%
+  distinct() %>%
+  mutate(INS_Inschrijvingsjaar = str_extract(sis_course_id, "(?<=_)(201[6-9]|202[0-9]|2030)(?=_)")) %>%
+  select(id, name, sis_course_id, INS_Inschrijvingsjaar)
+
+
+dfPDF_summarized <- dfPDF %>%
+  distinct() %>%
+  group_by(course_id) %>%
+  summarise(
+    n = n()
+  ) %>%
+  left_join(dfCourse_to_join, by = c(
+    "course_id" = "id"
+  ))
+
+
