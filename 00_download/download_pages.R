d02f1429790b2a12420675fc6b38927851ae35cb
tin900 <t.iwan@vu.nl>
2024-11-08 12:28:33 +0100

    replace: use local function

--- 00_download/download_pages.R
+++ 00_download/download_pages.R
@@ -23,17 +23,8 @@ tryCatch({
                                            base_dir = Sys.getenv("OUTPUT_DIR"),
                                            extension = "rds")
 
-  # Check for errors in the existing data
-  error_courses <- dfPages_filled %>%
-    dplyr::filter(!is.na(error)) %>%
-    pull(course_id) %>%
-    unique()
-
   df <- dfCourses %>%
-    dplyr::filter(!id %in% dfPages_filled$course_id | id %in% error_courses)
-
-  # df <- dfCourses %>%
-  #   dplyr::filter(!id %in% dfPages_filled$course_id)
+    dplyr::filter(!id %in% dfPages_filled$course_id)
 
   cat("Number of courses to process: ", nrow(df), "\n")
 
@@ -53,13 +44,44 @@ library(furrr)
 # Set up parallel processing
 plan(multisession, workers = parallel::detectCores() - 1)
 
+
+get_course_pages <- function(canvas, course_id, per_page = 100) {
+  # Construct the API endpoint URL
+  url <- paste0(canvas$base_url, "/api/v1/courses/", course_id, "/pages?per_page=", per_page)
+
+  # Make the API request
+  response <- httr::GET(url, httr::add_headers(Authorization = paste("Bearer", canvas$api_key)))
+
+  # Check the response status code
+  if (httr::status_code(response) != 200) {
+    stop("Failed to retrieve course pages. Please check your authentication and API endpoint.")
+  }
+
+  # Parse the response as JSON
+  pages <- httr::content(response, "text", encoding = "UTF-8") %>%
+    jsonlite::fromJSON(flatten = TRUE)
+
+  pages <- pages %>%
+    dplyr::mutate(page_body = purrr::map_chr(.data$page_id, ~get_page_content(canvas, course_id, .x)))
+
+  # Return the list of pages
+  return(pages)
+}
+
+
+
 dfPages <- df %>%
   pull(id) %>%
   future_map_dfr(~ {
     tryCatch(
       {
-        result <- vvcanvas::get_course_pages(canvas, .x)
-        result %>% mutate(course_id = .x)
+        result <- get_course_pages(canvas, .x)
+        if (nrow(result) == 1 && all(names(result) == "course_id")) {
+          # This means no pages were found
+          tibble(course_id = .x, no_pages = TRUE)
+        } else {
+          result
+        }
       },
       error = function(e) {
         tibble(course_id = .x, error = as.character(e))
@@ -67,14 +89,13 @@ dfPages <- df %>%
     )
   }, .progress = TRUE)
 
+
 if (exists("dfPages_filled")) {
   dfPages <- bind_rows(dfPages, dfPages_filled)
   dfPages <- dfPages %>%
     mutate(across(where(is.character), ~iconv(., to = "UTF-8"))) %>%
     distinct()
 }
-
-
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## WRITE & CLEAR ####
