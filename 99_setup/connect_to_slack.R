975cc1d436d1cd1e92bbc7d305a2b5a2761f6495
tin900 <t.iwan@vu.nl>
2024-11-13 09:34:02 +0100

    feat: when connection to slack create define helperfunction to generate messages

--- 99_setup/connect_to_slack.R
+++ 99_setup/connect_to_slack.R
@@ -23,6 +23,29 @@ if (!is.null(Sys.getenv("SLACK_TOKEN"))) {
     config_file = "~/.slackr",
     echo = FALSE
   )
+
+  ## define a function to prepare and send a summary report to Slack
+
+  prepare_and_send_summary <- function(main_data, previous_data, rows_to_process) {
+    # Prepare summary report
+    current_script <- basename(this.path::sys.path())
+
+    total_observations <- nrow(main_data)
+    previous_observations <- nrow(previous_data)
+    new_observations <- total_observations - previous_observations
+
+    summary_text <- paste("Script Summary:", current_script, "\n",
+                          "- Rows to process:", rows_to_process, "\n",
+                          "- Total observations:", total_observations, "\n",
+                          "- Previous observations:", previous_observations, "\n",
+                          "- New observations:", new_observations, "\n")
+
+    # Send summary report to Slack
+    slackr::slackr_msg(summary_text)
+
+    # Return the summary text (optional, in case you want to use it elsewhere)
+    return(summary_text)
+  }
 } else {
   message("No Slack token found")
 }
