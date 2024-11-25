c42a13fa99d6b4d8af68e0ea794657f6f09ba7f8
Hajo <h.b.bons@vu.nl>
2024-11-25 13:54:16 +0100

    fix: fixed assignment filters

--- 02_explore/abe/08_assignments.R
+++ 02_explore/abe/08_assignments.R
@@ -16,12 +16,15 @@ dfAssignments_uiteindelijk <- dfAssignments %>%
   dplyr::select(course_id, id, error) %>%
   dplyr::filter(course_id %in% unique_course_ids,
                 is.na(error)) %>%
-  dplyr::add_count(course_id, name="count_assignment_data")
+  dplyr::add_count(course_id, name="count_assignment_data") %>%
+  dplyr::distinct(course_id, count_assignment_data)
 
 dfCombined <- dfAssignments_uiteindelijk %>%
-  dplyr::left_join(dfABE_Uiteindelijk, by = "id") %>%
-  dplyr::select(course_id, id, WOTotal, count_assignment_data) %>%
-  dplyr::mutate(vergelijking_assignments = WOTotal - count_assignment_data)
+  dplyr::left_join(dfABE_Uiteindelijk, by = c("course_id" = "id")) %>%
+  dplyr::select(course_id, WOTotal, count_assignment_data) %>%
+  dplyr::mutate(vergelijking_assignments = WOTotal - count_assignment_data,
+                vergelijking_assignments_bool = WOTotal == count_assignment_data)
 
+tabyl(dfCombined$vergelijking_assignments_bool)
 
 vusa::write_file(dfCombined, "ABE_Assignments", destination = "20. Test/", save_rds = TRUE, save_csv = TRUE)
