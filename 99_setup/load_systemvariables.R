c2df1fa1fd25726f364d85c285df615e55e40474
tin900 <113692500+tin900@users.noreply.github.com>
2025-03-21 12:49:01 +0100

    update: documentation system variable

--- 99_setup/load_systemvariables.R
+++ 99_setup/load_systemvariables.R
@@ -17,6 +17,15 @@ set_all_envs <- function(var.name, var.value) {
 ## Lees in systeemvariabelen excel bestand
 to_set <- readxl::read_xlsx("G:/DSZ/SA2016/Datasets/Documentatie/Project/renviron.xlsx")
 
+## Set SHAREPOINT_DIR
+Sys.setenv(SHAREPOINt_DIR = paste0("C:/Users/", Sys.getenv("USERNAME"), "/Vrije Universiteit Amsterdam/"))
+
+Sys.setenv(DOCUMENTATION_DIR =
+             paste0(Sys.getenv("SHAREPOINt_DIR"), "Education Analytics - General/01_Documentatie/XX. Data Documentatie/"))
+
+Sys.setenv(MAP_TABLE_DIR =
+             paste0(Sys.getenv("SHAREPOINt_DIR"), "Education Analytics - General/01_Documentatie/XX. Data Documentatie/Mapping Tables/"))
+
 ## zet variabelen in R system variables
 pmap(list(to_set$variable, to_set$value), set_all_envs)
 
